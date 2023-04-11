7190542f6158f238e7afdf811b8f50c6f315ed03
Lucas Fernandes Lima <lucas.liima030@gmail.com>
2023-04-11 19:03:16 -0300

    feat: add to string for value object

--- src/@seedwork/domain/value-objects/unique-entity-id.vo.spec.ts
+++ src/@seedwork/domain/value-objects/unique-entity-id.vo.spec.ts
@@ -1,5 +1,5 @@
 import { validate as uuidValidate } from 'uuid';
-import InvalidUuidError from '../errors/invalid-uuid.error';
+import InvalidUuidError from '../../errors/invalid-uuid.error';
 import UniqueEntityId from './unique-entity-id.vo';
 
 describe('UniqueEntityId Unit Tests', () => {
@@ -13,13 +13,13 @@ describe('UniqueEntityId Unit Tests', () => {
   it('should accept a uuid passed in constructor', () => {
     const uuid = '48fac1a8-b6c0-40ab-8c58-e737b4635595';
     const vo = new UniqueEntityId(uuid);
-    expect(vo.id).toBe(uuid);
+    expect(vo.value).toBe(uuid);
     expect(validateSpy).toHaveBeenCalled();
   });
 
   it('should generate a uuid when not passed in the constructor', () => {
     const vo = new UniqueEntityId();
-    expect(uuidValidate(vo.id)).toBeTruthy()
+    expect(uuidValidate(vo.value)).toBeTruthy()
     expect(validateSpy).toHaveBeenCalled();
   });
 });
