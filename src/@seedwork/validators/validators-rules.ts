ecf9275b9e27a06d0e994b35e36d8ea63860e8ac
Lucas Fernandes Lima <lucas.liima030@gmail.com>
2023-04-13 08:11:14 -0300

    feat: add isEmpty and boolean validator

--- src/@seedwork/validators/validators-rules.ts
+++ src/@seedwork/validators/validators-rules.ts
@@ -8,7 +8,7 @@ export default class ValidatorRules {
     return new ValidatorRules(value, property);
   }
 
-  public required(): this {
+  public required(): Omit<this, 'required'> { // O Omit serve para não chamar o método duas vezes. Ex. ValidatorsRules.required().required()
     if (this.value === null || this.value === undefined || this.value === '') {
       throw new ValidationError(`The ${this.property} is required`);
     }
@@ -16,19 +16,32 @@ export default class ValidatorRules {
     return this;
   }
 
-  public string(): this {
-    if (typeof this.value !== 'string') {
+  public string(): Omit<this, 'string'> {
+    if (!isEmpty(this.value) && typeof this.value !== 'string') {
       throw new ValidationError(`The ${this.property} must be a string`);
     }
 
     return this;
   }
 
-  public maxLength(max: number): this {
-    if (this.value.length > max) {
+  public maxLength(max: number): Omit<this, 'maxLength'> {
+    if (!isEmpty(this.value) && this.value.length > max) {
       throw new ValidationError(`The ${this.property} must be less or equal than ${max} characters`);
     }
 
     return this;
   }
+
+  public boolean(): Omit<this, 'boolean'> {
+    if (!isEmpty(this.value) && typeof this.value !== 'boolean') {
+      throw new ValidationError(`The ${this.property} must be a boolean`);
+    }
+    
+    return this;
+  }
+
+}
+
+export function isEmpty(value: any): boolean {
+  return value === null || value === undefined;
 }
