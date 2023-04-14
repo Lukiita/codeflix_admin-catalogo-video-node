3f177e63921c0f638641544254f5e5accbea2187
Lucas Fernandes Lima <lucas.liima030@gmail.com>
2023-04-14 09:07:09 -0300

    feat: create in memory repository

--- src/@seedwork/domain/repostiory/repository-contracts.ts
+++ src/@seedwork/domain/repostiory/repository-contracts.ts
@@ -3,7 +3,7 @@ import UniqueEntityId from '../value-objects/unique-entity-id.vo';
 
 export interface IRepository<E extends Entity> {
   insert(entity: E): Promise<void>;
-  findById(id: string | UniqueEntityId): Promise<E[]>;
+  findById(id: string | UniqueEntityId): Promise<E>;
   findAll(): Promise<E[]>;
   update(entity: E): Promise<void>;
   delete(id: string | UniqueEntityId): Promise<void>;
