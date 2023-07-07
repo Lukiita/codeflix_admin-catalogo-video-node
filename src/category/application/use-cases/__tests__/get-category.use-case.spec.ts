ed9bd92d43afbc0f4393cdef75a4c09f929f314a
Lucas Fernandes Lima <lucas.liima030@gmail.com>
2023-07-07 18:37:22 -0300

    test: add tests for get category use case

---
+++ src/category/application/use-cases/__tests__/get-category.use-case.spec.ts
@@ -0,0 +1,39 @@
+import NotFoundError from '../../../../@seedwork/domain/errors/not-found.error';
+import { Category } from '../../../domain/entities/category';
+import CategoryInMemoryRepository from '../../../infra/repositories/category-in-memory.repository';
+import GetCategoryUseCase from '../get-category.use-case';
+
+describe('GetCategoryUseCase Unit Tests', () => {
+  let useCase: GetCategoryUseCase;
+  let repository: CategoryInMemoryRepository;
+
+  beforeEach(() => {
+    repository = new CategoryInMemoryRepository();
+    useCase = new GetCategoryUseCase(repository);
+  });
+
+  it('should throws error when entity not found', () => {
+    expect(() => useCase.execute({ id: 'fake_id' })).rejects.toThrow(
+      new NotFoundError('Entity Not Found using ID fake_id')
+    );
+  });
+
+  it('should returns a category', async () => {
+    const items = [
+      new Category({ name: 'Category' })
+    ];
+    repository.items = items;
+
+    const spyFindById = jest.spyOn(repository, 'findById');
+    const output = await useCase.execute({ id: repository.items[0].id });
+    expect(output).toStrictEqual({
+      id: items[0].id,
+      name: 'Category',
+      description: null,
+      is_active: true,
+      created_at: items[0].created_at
+    });
+    expect(spyFindById).toBeCalledTimes(1);
+    expect(spyFindById).toBeCalledWith(items[0].id);
+  });
+});
