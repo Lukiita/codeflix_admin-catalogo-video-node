fad29f082e69e91ce16d7f3ffb0b25b1826ba711
Lucas Fernandes Lima <lucas.liima030@gmail.com>
2023-07-07 18:37:11 -0300

    feat: add get category use case

---
+++ src/category/application/use-cases/get-category.use-case.ts
@@ -0,0 +1,23 @@
+import CategoryRepository from '../../domain/repositories/category.repository';
+import { CategoryOutputDto } from '../dto/category-output.dto';
+
+export default class GetCreateCategoryUseCase {
+
+  constructor(private categoryRepo: CategoryRepository.Repository) {}
+
+  async execute(input: Input): Promise<CategoryOutputDto> {
+    const entity = await this.categoryRepo.findById(input.id);
+
+    return {
+      id: entity.id,
+      name: entity.name,
+      description: entity.description,
+      is_active: entity.is_active,
+      created_at: entity.created_at
+    }
+  }
+}
+
+export type Input = {
+  id: string;
+}
