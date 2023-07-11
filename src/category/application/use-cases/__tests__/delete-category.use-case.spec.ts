005b66e7ba99a807eb052a0f5810478784b259a7
Lucas Fernandes Lima <lucas.liima030@gmail.com>
2023-07-11 14:33:23 -0300

    test: add tests for delete category use case

---
+++ src/category/application/use-cases/__tests__/delete-category.use-case.spec.ts
@@ -0,0 +1,33 @@
+import NotFoundError from '../../../../@seedwork/domain/errors/not-found.error';
+import { Category } from '../../../../category/domain/entities/category';
+import CategoryInMemoryRepository from '../../../infra/repositories/category-in-memory.repository';
+import DeleteCategoryUseCase from '../delete-category.use-case';
+
+describe('DeleteCategoryUseCase Unit Tests', () => {
+  let useCase: DeleteCategoryUseCase;
+  let repository: CategoryInMemoryRepository;
+
+  beforeEach(() => {
+    repository = new CategoryInMemoryRepository();
+    useCase = new DeleteCategoryUseCase(repository);
+  });
+
+  it('should throws error when entity not found', () => {
+    expect(() => useCase.execute({ id: 'fake_id'})).rejects.toThrow(
+      new NotFoundError('Entity Not Found using ID fake_id')
+    );
+  });
+
+  it('should delete a category', async () => {
+    const spyDelete = jest.spyOn(repository, 'delete');
+    const entity = new Category({ name: 'Movie' });
+    const entity2 = new Category({ name: 'Cartoon' });
+    repository.items = [entity, entity2];
+    await useCase.execute({ id: entity.id });
+    expect(spyDelete).toBeCalledTimes(1);
+    expect(repository.items).toHaveLength(1);
+
+    await useCase.execute({ id: entity2.id });
+    expect(repository.items).toHaveLength(0);
+  });
+});
