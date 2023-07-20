3f8e6bae029a2eb8501eb9c2bb052e8a2f26b74c
Lucas Fernandes Lima <lucas.liima030@gmail.com>
2023-07-20 17:13:51 -0300

    chore: add provider category

--- src/nestjs/src/categories/categories.module.ts
+++ src/nestjs/src/categories/categories.module.ts
@@ -1,51 +1,12 @@
 import { Module } from '@nestjs/common';
-import { CreateCategoryUseCase, DeleteCategoryUseCase, GetCategoryUseCase, ListCategoriesUseCase, UpdateCategoryUseCase } from 'codeflix/category/application';
-import { CategoryInMemoryRepository } from 'codeflix/category/infra';
-import CategoryRepository from 'codeflix/dist/category/domain/repositories/category.repository';
 import { CategoriesController } from './categories.controller';
+import { CATEGORY_PROVIDERS } from './category.providers';
 
 @Module({
   controllers: [CategoriesController],
   providers: [
-    {
-      provide: 'CategoryRepository',
-      useClass: CategoryInMemoryRepository
-    },
-    {
-      provide: CreateCategoryUseCase.UseCase,
-      useFactory: (categoryRepo: CategoryRepository.Repository) => {
-        return new CreateCategoryUseCase.UseCase(categoryRepo);
-      },
-      inject: ['CategoryRepository']
-    },
-    {
-      provide: ListCategoriesUseCase.UseCase,
-      useFactory: (categoryRepo: CategoryRepository.Repository) => {
-        return new ListCategoriesUseCase.UseCase(categoryRepo);
-      },
-      inject: ['CategoryRepository']
-    },
-    {
-      provide: GetCategoryUseCase.UseCase,
-      useFactory: (categoryRepo: CategoryRepository.Repository) => {
-        return new GetCategoryUseCase.UseCase(categoryRepo);
-      },
-      inject: ['CategoryRepository']
-    },
-    {
-      provide: UpdateCategoryUseCase.UseCase,
-      useFactory: (categoryRepo: CategoryRepository.Repository) => {
-        return new UpdateCategoryUseCase.UseCase(categoryRepo);
-      },
-      inject: ['CategoryRepository']
-    },
-    {
-      provide: DeleteCategoryUseCase.UseCase,
-      useFactory: (categoryRepo: CategoryRepository.Repository) => {
-        return new DeleteCategoryUseCase.UseCase(categoryRepo);
-      },
-      inject: ['CategoryRepository']
-    },
+    ...Object.values(CATEGORY_PROVIDERS.REPOSITORIES),
+    ...Object.values(CATEGORY_PROVIDERS.USE_CASES),
   ]
 })
 export class CategoriesModule { }
